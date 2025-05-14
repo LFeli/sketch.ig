0111057e473f574d06049fb925859469d40b5f28
Lucas Araújo <lucasfelipemk@gmail.com>
2025-05-13 22:44:30 -0300

    :wrench: chore: add site configuration with metadata and description

---
+++ src/config/site.ts
@@ -0,0 +1,10 @@
+export const siteConfig = {
+  name: 'flown.ne',
+  url: 'https://github.com/LFeli/sketch.ig',
+  description:
+    'sketch.ig is a personal playground for exploring UI ideas, building miscellaneous components, and testing frontend interactions. Designed for speed and creativity, it’s a living lab of micro experiments—ideal for shaping reusable logic, visuals, and flows outside the boundaries of a main product.',
+  author: {
+    name: 'Lucas Araújo | LFeli',
+    github: 'https://github.com/LFeli',
+  },
+}
