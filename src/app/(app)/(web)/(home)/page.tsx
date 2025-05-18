2e192436ec3a9bbfc01da01ffca57c1479c31865
Lucas Araújo <lucasfelipemk@gmail.com>
2025-05-18 01:13:21 -0300

    :sparkles: feat: implement HomePage with CallToAction component and remove old page

---
+++ src/app/(app)/(web)/(home)/page.tsx
@@ -0,0 +1,10 @@
+import { CallToAction } from './components/call-to-action'
+
+export default function HomePage() {
+  return (
+    <main className="h-full">
+      <CallToAction />
+      <div>another section</div>
+    </main>
+  )
+}
