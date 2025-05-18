79d7a0e80beec8988cc41b948f6d7dc3fc5a7e68
Lucas Araújo <lucasfelipemk@gmail.com>
2025-05-18 02:39:45 -0300

    :sparkles: feat: integrate Features component into HomePage for enhanced content presentation

--- src/app/(app)/(web)/(home)/page.tsx
+++ src/app/(app)/(web)/(home)/page.tsx
@@ -1,10 +1,11 @@
 import { CallToAction } from './components/call-to-action'
+import { Features } from './components/features'
 
 export default function HomePage() {
   return (
     <main className="h-full">
       <CallToAction />
-      <div>another section</div>
+      <Features />
     </main>
   )
 }
