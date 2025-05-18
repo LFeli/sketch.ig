f9adcf7f2592b87b4be18d31ad895de1708e0723
Lucas Araújo <lucasfelipemk@gmail.com>
2025-05-18 02:39:39 -0300

    :sparkles: feat: add Features component with animations and Badge for enhanced presentation

---
+++ src/app/(app)/(web)/(home)/components/features.tsx
@@ -0,0 +1,57 @@
+'use client'
+
+import { motion } from 'motion/react'
+
+import { Badge } from '@/components/ui/badge'
+import { animationContainer, animationItem } from '@/constants/animation'
+import { SparklesIcon } from 'lucide-react'
+
+export function Features() {
+  return (
+    <motion.div
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: true, amount: 0.4 }}
+      variants={animationContainer}
+    >
+      <section className="relative flex min-h-screen flex-col items-center justify-between overflow-hidden px-4 py-16 lg:py-24">
+        <header className="flex flex-col items-center justify-center space-y-6">
+          {/* tag badge */}
+          <Badge
+            variant={'secondary'}
+            className="gap-2 rounded-full px-3 py-1.25 font-mono"
+            asChild
+          >
+            <motion.span variants={animationItem}>
+              Features
+              <SparklesIcon />
+            </motion.span>
+          </Badge>
+
+          <motion.h2
+            variants={animationItem}
+            className="bg-gradient-to-b from-neutral-900 to-neutral-700 bg-clip-text text-center font-bold font-sans text-3xl text-transparent leading-tight tracking-tight md:text-5xl lg:text-6xl dark:from-neutral-600 dark:to-white"
+          >
+            A Playground, Not a Product
+          </motion.h2>
+
+          <motion.p
+            variants={animationItem}
+            className="mx-auto max-w-xl text-center text-neutral-700 text-sm md:text-lg dark:text-neutral-400"
+          >
+            sketch.ig is my personal collection of micro-components, creative
+            snippets, and experiments. It's where ideas get broken, rebuilt, and
+            reimagined—fast
+          </motion.p>
+        </header>
+
+        <article>middle section</article>
+
+        <footer className="mx-auto max-w-xl text-center text-neutral-700 text-xs lg:text-sm dark:text-neutral-400">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quae
+          ipsam beatae blanditiis sed vel vero nihil consectetur expedita.
+        </footer>
+      </section>
+    </motion.div>
+  )
+}
