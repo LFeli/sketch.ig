c38c0d2e1f24384a682c5a2d5562a40e2fb86df8
Lucas Araújo <lucasfelipemk@gmail.com>
2025-05-25 00:26:53 -0300

    :sparkles: feat: add SiteFooter component to WebLayout for improved page structure

--- src/app/(app)/(web)/layout.tsx
+++ src/app/(app)/(web)/layout.tsx
@@ -1,7 +1,14 @@
+import { SiteFooter } from '@/components/layout/footer'
+
 interface WebLayoutProps {
   children: React.ReactNode
 }
 
 export default function WebLayout({ children }: WebLayoutProps) {
-  return <div>{children}</div>
+  return (
+    <div>
+      {children}
+      <SiteFooter />
+    </div>
+  )
 }
