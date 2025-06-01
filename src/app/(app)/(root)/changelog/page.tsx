a3a7637a6bd1eccc2ccddb216356641b5ac7b2f6
Lucas Araújo <lucasfelipemk@gmail.com>
2025-05-31 23:54:51 -0300

    feat: add ChangelogPage component with call-to-action for user engagement

---
+++ src/app/(app)/(root)/changelog/page.tsx
@@ -0,0 +1,9 @@
+import { ChangelogCallToAction } from './components/call-to-action'
+
+export default function ChangelogPage() {
+  return (
+    <main className="h-full">
+      <ChangelogCallToAction />
+    </main>
+  )
+}
